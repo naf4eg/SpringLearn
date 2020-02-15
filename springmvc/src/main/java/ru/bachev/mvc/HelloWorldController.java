a13ffcaab1a036ab1b61bb2cac52fed7018ff677
Бачев Константин Николаевич <Bachev.K.N@sberbank.ru>
2020-02-15 16:21:49 +0300

    - used @RequestParam

--- springmvc/src/main/java/ru/bachev/mvc/HelloWorldController.java
+++ springmvc/src/main/java/ru/bachev/mvc/HelloWorldController.java
@@ -3,6 +3,7 @@ package ru.bachev.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -19,13 +20,26 @@ public class HelloWorldController {
         return "helloworld";
     }
 
-    @RequestMapping("/processFormVersionTwo")
+    @RequestMapping("/processFormVersionOne")
     public String say(HttpServletRequest request, Model model) {
 
         //read the request parameter rom the HTML form
         String theName = request.getParameter("studentName").toUpperCase();
         //create the message
         String result = "Yo! " + theName;
+        //add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String saytwo(
+            @RequestParam("studentName") String theName,
+            Model model) {
+
+        //create the message
+        String result = "Yo! FORM 2V. " + theName.toUpperCase();
         //add message to the model
         model.addAttribute("message", result);
 
--- springmvc/src/main/kotlin/ru/bachev/mvc/HelloWorldControllerKotlin.kt
+++ springmvc/src/main/kotlin/ru/bachev/mvc/HelloWorldControllerKotlin.kt
@@ -3,6 +3,7 @@ package ru.bachev.mvc
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import javax.servlet.http.HttpServletRequest
 
 @Controller
@@ -17,10 +18,20 @@ class HelloWorldControllerKotlin {
         return "helloworld"
     }
 
-    @RequestMapping("/kt/processFormVersionTwo")
-    fun say(request: HttpServletRequest, model: Model): String {
+    @RequestMapping("/kt/processFormVersion")
+    fun sayOne(request: HttpServletRequest, model: Model): String {
         val name: String = request.getParameter("studentName").toUpperCase()
         val message: String = "YO KOTLIN! $name"
+        model.addAttribute("message", message)
+
+        return "helloworld"
+    }
+
+    @RequestMapping("/kt/processFormVersionTwo")
+    fun sayTwo(@RequestParam("studentName") name: String,
+            model: Model): String {
+
+        val message: String = "YO KOTLIN, FORM 2V! ${name.toUpperCase()}"
         model.addAttribute("message", message)
 
         return "helloworld"
