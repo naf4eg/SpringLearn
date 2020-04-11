cb9089baf7946223414a08ae2ac349881d28e8a2
naf4eg <naf4eg@yandex.ru>
2020-04-12 00:44:20 +0300

    - Learn Hibernate. Test connection JDBC to PostgreSQL

---
+++ hibernate/src/main/java/ru/bachev/hebernate/TestJDBC.java
@@ -0,0 +1,22 @@
+package ru.bachev.hebernate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJDBC {
+    static String jdbsUrl = "jdbc:postgresql://localhost:5432/hb_student_tracker?ssl=false";
+    static String user = "postgres";
+    static String pass = "25111987";
+
+    public static void main(String[] args) {
+        {
+            try {
+                Connection connection = DriverManager.getConnection(jdbsUrl, user, pass);
+                System.out.println("Connection successful!!!  " + connection.getCatalog());
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
